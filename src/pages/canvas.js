b9c9da9d2dd50b5430f6163fda3093f105fee690
sidahmed <ahorob57@gmail.com>
2020-11-26 22:19:05 +0100

    remov the space from acos

--- src/pages/canvas.js
+++ src/pages/canvas.js
@@ -105,7 +105,7 @@ const Canvas = ({ coordinates }) => {
         const U_magnitude = Math.sqrt(((Ux)**2) + ((Uy)**2))
         const V_magnitude = Math.sqrt(((Vx)**2) + ((Vy)**2))
 
-        const cos_theta = Math.acos (UV_dot_Product/(U_magnitude * V_magnitude))
+        const cos_theta = Math.acos(UV_dot_Product/(U_magnitude * V_magnitude))
         const theta = cos_theta * (180 / Math.PI)
 
         return theta
