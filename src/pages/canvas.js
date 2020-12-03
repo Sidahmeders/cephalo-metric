ed99bfcca213ab2bcf243f2507a57fd9314a513b
sidahmed <ahorob57@gmail.com>
2020-12-03 02:03:45 +0100

    clear m

--- src/pages/canvas.js
+++ src/pages/canvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 
 const Drags = () => {
@@ -92,7 +92,6 @@ const Drags = () => {
                     lines.push(new Line(...tempLineValues))
                     tempLineValues = []
                 }
-
                 drawCircles()
                 isPointSelected = false
             }
