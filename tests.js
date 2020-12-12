267d0e1972f9ad1e76c13f3048dec154caa65372
sidahmed <ahorob57@gmail.com>
2020-12-12 20:18:22 +0100

    backtarck solution

--- tests.js
+++ tests.js
@@ -8,13 +8,21 @@
 [3,4]
 */
 
-
 var combine = function(n, k) {
-    
-    let i = 0
-    while (i < k) {
-        
-        k++
+    let ans = []
+    backtracking(ans,[],1,n,k)
+    return ans
+}
+
+var backtracking = function(ans,tmp,start,n,k) {
+    if(tmp.length == k){
+      ans.push(Array.from(tmp))
+      return
+    }
+    for(let i=start;i<=n;++i){
+      tmp.push(i)
+      backtracking(ans,tmp,i+1,n,k)
+      tmp.pop()
     }
 }
 
