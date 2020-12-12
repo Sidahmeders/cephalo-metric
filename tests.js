4e8e339d9dc6ab377e5b723500e0a81e839e61ce
sidahmed <ahorob57@gmail.com>
2020-12-12 20:06:47 +0100

    combine leetcoe

---
+++ tests.js
@@ -0,0 +1,21 @@
+// write your leetcode tests here
+/*
+[1,2] 
+[1,3] 
+[1,4] 
+[2,3] 
+[2,4] 
+[3,4]
+*/
+
+
+var combine = function(n, k) {
+    
+    let i = 0
+    while (i < k) {
+        
+        k++
+    }
+}
+
+console.log(combine(4, 2))
